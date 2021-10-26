7d5ce4b704fa1c541637b15f12221bc4f391814a
NewPirateOfUASeas <75931217+NewPirateOfUASeas@users.noreply.github.com>
2021-10-26 21:31:44 +0300

    a dumb attempt with symmetric while

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,8 @@
 	<input type="number" v-model="saturationValue" />
 	Lightness:
 	<input type="number" v-model="lightnessValue" />
-
+	Alpha:
+	<input type="number" v-model="alphaValue" />
 	<div>
 		<h1>Before:</h1>
 		<div class="colorSquare" v-for="color in themeColors" :key="color" :style="`background-color: ${color}`"></div>
@@ -33,10 +34,12 @@ export default {
 	name: "App",
 	data() {
 		return {
-			themeColors: [],
-			hueValue: 100,
-			saturationValue: 100,
-			lightnessValue: 100
+			allowedRange: 0.1,
+			themeColors: ["#000000"],
+			hueValue: 0,
+			saturationValue: 0,
+			lightnessValue: 0,
+			alphaValue: 1
 		};
 	},
 	mounted() {
@@ -48,21 +51,53 @@ export default {
 		this.themeColors = Array.from(themeColorsSet);
 	},
 	methods: {
-		shiftColor(val) {
-			const rgb = colorutil.hex.to.rgb(val);
+		convertToHSL(hexFormatedColor) {
+			const rgb = colorutil.hex.to.rgb(hexFormatedColor);
 			const hsl = colorutil.rgb.to.hsl(rgb);
-			hsl.h *= this.hueValue / 100;
-			hsl.s *= this.saturationValue / 100;
-			hsl.l *= this.lightnessValue / 100;
-			const rgba = colorutil.hsl.to.csshsl(hsl);
-			return rgba;
+			hsl.h += this.hueValue / 100;
+			hsl.s += this.saturationValue / 100;
+			hsl.l += this.lightnessValue / 100;
+			hsl.a += this.alphaValue / 10;
+			return hsl; // return colorutil.hsl.to.csshsl(hsl);
+		},
+		groupColor(HSLFormattedColor) {
+			return HSLFormattedColor.sort(function (a, b) {
+				return a.h - b.h;
+			});
+		},
+		splitColorsByHueRanges(arrayWithSortedHSL) {
+			let highestAmountOfColorsInRange = 0;
+			function callee(element, propIndex) {
+				let localIndex = 0;
+				let amountOfColorsInRangeOfCurrentRing = 0;
+				while (
+					element.h - arrayWithSortedHSL[propIndex + localIndex].h < this.allowedRange ||
+					element.h - arrayWithSortedHSL[propIndex - localIndex].h > -this.allowedRange
+				) {
+					amountOfColorsInRangeOfCurrentRing++;
+					localIndex++;
+				}
+				return amountOfColorsInRangeOfCurrentRing;
+			}
+			for (let index = 0; index < arrayWithSortedHSL.length; index++) {
+				const element = arrayWithSortedHSL[index];
+				if (highestAmountOfColorsInRange < callee(element, index)) {
+					highestAmountOfColorsInRange++;
+				}
+			}
 		}
 	},
 	computed: {
 		turnedColor() {
 			return this.themeColors.map((color) => {
-				return this.shiftColor(color);
+				return this.convertToHSL(color);
 			});
+		},
+		sortedHSL() {
+			return this.groupColor(this.turnedColor);
+		},
+		colorsSplittedByHueRanges() {
+			return this.splitColorsByHueRanges(this.sortedHSL);
 		}
 	}
 };
